f26863130218ebb1c9e93e3522ac09158bbb7826
dikiytechies <160992549+dikiytechies@users.noreply.github.com>
2026-05-27 16:05:06 +0300

    ihateintellijidea

---
+++ src/main/java/com/dikiytechies/ironage/util/GameplayEventHandler.java
@@ -0,0 +1,23 @@
+package com.dikiytechies.ironage.util;
+
+import com.dikiytechies.ironage.IronAge;
+import com.dikiytechies.ironage.world.WorldAge;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.player.Player;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.event.entity.living.LivingEvent;
+import net.neoforged.neoforge.event.entity.player.PlayerInteractEvent;
+
+import static com.dikiytechies.ironage.IronAge.MOD_ID;
+
+@EventBusSubscriber(modid = MOD_ID)
+public class GameplayEventHandler {
+    @SubscribeEvent
+    public static void onInteract(LivingEvent.LivingJumpEvent event) {
+        if (!event.getEntity().level().isClientSide() && event.getEntity() instanceof Player) {
+            System.out.println(WorldAge.get(event.getEntity().level().getServer()).get().name());
+            WorldAge.get(event.getEntity().level().getServer()).set(WorldAge.WorldStage.values()[((WorldAge.get(event.getEntity().level().getServer()).get().ordinal() + 1) % 3)]);
+        }
+    }
+}
---
+++ src/main/java/com/dikiytechies/ironage/world/WorldAge.java
@@ -0,0 +1,50 @@
+package com.dikiytechies.ironage.world;
+
+import net.minecraft.core.HolderLookup;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.world.level.saveddata.SavedData;
+import net.minecraft.world.level.storage.DimensionDataStorage;
+
+public class WorldAge extends SavedData {
+    private WorldStage worldStage;
+
+    public WorldAge() {
+        this.worldStage = WorldStage.PRE_STONE;
+    }
+
+    public WorldStage get() {
+        return worldStage;
+    }
+
+    public void set(WorldStage stage) {
+        worldStage = stage;
+        setDirty();
+    }
+
+    @Override
+    public CompoundTag save(CompoundTag compoundTag, HolderLookup.Provider provider) {
+        compoundTag.putString("WorldStage", worldStage.name());
+        return compoundTag;
+    }
+
+    public static WorldAge load(CompoundTag compoundTag, HolderLookup.Provider provider) {
+        WorldAge data = new WorldAge();
+        data.worldStage = WorldStage.valueOf(compoundTag.getString("WorldStage"));
+
+        return data;
+    }
+
+    public static WorldAge get(MinecraftServer server) {
+        DimensionDataStorage storage = server.overworld().getDataStorage();
+
+        return storage.computeIfAbsent(new Factory<>(WorldAge::new, WorldAge::load), "world_age");
+    }
+
+
+    public enum WorldStage {
+        DEFAULT,
+        PRE_STONE,
+        PRE_IRON
+    }
+}
