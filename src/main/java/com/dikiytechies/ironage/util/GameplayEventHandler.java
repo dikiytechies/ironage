2614092018a5c89eceeb728b86afd08d2f3d719a
dikiytechies <l555777801@mail.ru>
2026-05-30 19:53:35 +0300

    cancelling iron drops from entities

--- src/main/java/com/dikiytechies/ironage/util/GameplayEventHandler.java
+++ src/main/java/com/dikiytechies/ironage/util/GameplayEventHandler.java
@@ -1,10 +1,19 @@
 package com.dikiytechies.ironage.util;
 
 import com.dikiytechies.ironage.world.WorldAge;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.event.entity.living.LivingDropsEvent;
 import net.neoforged.neoforge.event.entity.living.LivingEvent;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
 
 import static com.dikiytechies.ironage.IronAge.MOD_ID;
 
@@ -16,6 +25,24 @@ public class GameplayEventHandler {
         if (!event.getEntity().level().isClientSide() && event.getEntity() instanceof Player) {
             System.out.println(WorldAge.get(event.getEntity().level().getServer()).get().name());
             WorldAge.get(event.getEntity().level().getServer()).set(WorldAge.WorldStage.values()[((WorldAge.get(event.getEntity().level().getServer()).get().ordinal() + 1) % 3)]);
+        }
+    }
+
+    @SubscribeEvent
+    public static void onLivingDrops(LivingDropsEvent event) {
+        transformIronIngots(event);
+    }
+
+    private static void transformIronIngots(LivingDropsEvent event) {
+        LivingEntity entity = event.getEntity();
+        if (!(entity instanceof ServerPlayer) && !WorldAge.get(entity.getServer()).get().equals(WorldAge.WorldStage.DEFAULT)) {
+            event.getDrops().forEach(item -> {
+                if (item.getItem().getItem().equals(Items.IRON_INGOT)) {
+                    ItemStack newStack = new ItemStack(Items.IRON_NUGGET);
+                    newStack.setCount(item.getItem().getCount());
+                    item.setItem(newStack);
+                }
+            });
         }
     }
 }
