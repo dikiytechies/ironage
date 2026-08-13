eb5a5af4e395d78735a54f4d773dcc13af45d70c
dikiytechies <l555777801@mail.ru>
2026-08-13 14:36:37 +0300

    manual equipment cancel

---
+++ src/main/java/com/dikiytechies/ironage/mixin/ArmorBanMixin.java
@@ -0,0 +1,32 @@
+package com.dikiytechies.ironage.mixin;
+
+import com.dikiytechies.ironage.IronAgeConfig;
+import com.dikiytechies.ironage.world.ClientWorldAge;
+import com.dikiytechies.ironage.world.WorldAge;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.ItemStack;
+import net.neoforged.neoforge.common.extensions.IItemStackExtension;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(IItemStackExtension.class)
+public interface ArmorBanMixin {
+    @Shadow
+    ItemStack self();
+
+    @Inject(method = "canEquip", at = @At("HEAD"), cancellable = true)
+    default void preventEquip(EquipmentSlot armorType, LivingEntity entity, CallbackInfoReturnable<Boolean> cir) {
+        WorldAge.WorldStage stage = entity.level().isClientSide()? ClientWorldAge.getInstance().getStage(): WorldAge.get(entity.level().getServer()).get();
+        var config = IronAgeConfig.CONFIG;
+        boolean shouldProcess = stage.equals(WorldAge.WorldStage.PRE_STONE)? config.getPreStoneBanned().contains(self().getItem()):
+                stage.equals(WorldAge.WorldStage.PRE_IRON) && (config.getPreStoneBanned().contains(self().getItem()) || config.getPreIronBanned().contains(self().getItem()));
+        if (shouldProcess) {
+            cir.setReturnValue(false);
+            cir.cancel();
+        }
+    }
+}
